3ca1fa8ae774f2ecd28a5d684a832f20e2ed4992
jinio <jinioh88@gmail.com>
2020-06-02 12:09:05 +0900

    REST API 데이터 가져오기

---
+++ src/main/java/tacos/web/api/DesignTacoAPIController.java
@@ -0,0 +1,38 @@
+package tacos.web.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tacos.Taco;
+import tacos.data.TacoRepository;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/design", produces = "application/json")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+public class DesignTacoAPIController {
+    private final TacoRepository tacoRepository;
+//    private final EntityLinks entityLinks;
+
+    @GetMapping("/recent")
+    public Iterable<Taco> recentTacos() {
+        PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+
+        return tacoRepository.findAll(page).getContent();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        Optional<Taco> optTaco = tacoRepository.findById(id);
+        if(optTaco.isPresent()) {
+            return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+}
